7db0855e1582540cb28354b864adb2ad7c151eb5
CHAE YEONG HUN <proin0312@gmail.com>
2013-01-07 01:38:23 +0900

    APK

--- src/com/proinlab/gdlapp/DeveloperKey.java
+++ src/com/proinlab/gdlapp/DeveloperKey.java
@@ -2,17 +2,8 @@
 
 package com.proinlab.gdlapp;
 
-/**
- * Static container class for holding a reference to your YouTube Developer Key.
- */
 public class DeveloperKey {
-
-  /**
-   * Please replace this with a valid API key which is enabled for the 
-   * YouTube Data API v3 service. Go to the 
-   * <a href="https://code.google.com/apis/console/">Google APIs Console</a> to
-   * register a new developer key.
-   */
-  public static final String DEVELOPER_KEY = "AIzaSyBfwGhens_LTFL2iFk01AKo8Sjcv1tnsbI";
+	// Set Your Key
+	public static final String DEVELOPER_KEY = "AIzaSyBfwGhens_LTFL2iFk01AKo8Sjcv1tnsbI";
 
 }
