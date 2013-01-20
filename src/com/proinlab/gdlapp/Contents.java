23a7a96cf332f751eae3b4e6d9c8446123c5ea01
proin <proin0312@gmail.com>
2013-01-20 17:47:20 +0900

    KeyCode for GTV

--- src/com/proinlab/gdlapp/Contents.java
+++ src/com/proinlab/gdlapp/Contents.java
@@ -1,15 +1,16 @@
 package com.proinlab.gdlapp;
 
-import android.view.ViewGroup.LayoutParams;
 import android.annotation.TargetApi;
 import android.app.ActionBar;
 import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.AttributeSet;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.FrameLayout;
 
 import com.google.android.youtube.player.YouTubePlayer;
@@ -21,6 +22,7 @@ public class Contents extends YouTubeFailureRecoveryActivity implements
 
 	private ActionBarPaddedFrameLayout viewContainer;
 	private YouTubePlayerFragment playerFragment;
+	private YouTubePlayer player;
 
 	private String Title;
 	private String YouTubeId;
@@ -30,6 +32,21 @@ public class Contents extends YouTubeFailureRecoveryActivity implements
 	public static final String EXTRAS_CONTENTS_DATE = "EXTRAS_CONTENTS_DATE";
 	public static final String EXTRAS_CONTENTS_THUMBNAIL = "EXTRAS_CONTENTS_THUMBNAIL";
 
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		switch (keyCode) {
+		case KeyEvent.KEYCODE_MEDIA_PAUSE:
+			if(player.isPlaying())
+				player.pause();
+			return true;
+		case KeyEvent.KEYCODE_MEDIA_PLAY:
+			if(!player.isPlaying())
+				player.play();
+			return true;
+		default:
+			return super.onKeyDown(keyCode, event);
+		}
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -37,7 +54,7 @@ public class Contents extends YouTubeFailureRecoveryActivity implements
 
 		YouTubeId = getIntent().getExtras().getString(EXTRAS_CONTENTS_LINK);
 		Title = getIntent().getExtras().getString(EXTRAS_CONTENTS_TITLE);
-		
+
 		getActionBar().setBackgroundDrawable(new ColorDrawable(0xAA000000));
 		getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
 		getActionBar().setTitle(Title);
@@ -58,7 +75,8 @@ public class Contents extends YouTubeFailureRecoveryActivity implements
 		player.setFullscreen(true);
 		player.addFullscreenControlFlag(YouTubePlayer.FULLSCREEN_FLAG_CUSTOM_LAYOUT);
 		player.setOnFullscreenListener(this);
-
+		this.player = player;
+		
 		if (!wasRestored) {
 			player.loadVideo(YouTubeId);
 		}
