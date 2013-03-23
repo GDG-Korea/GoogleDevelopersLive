46d376201281a899fec7a4b3da5f663337af4ba6
Shaka Huang <shaka@cs03.org>
2013-03-23 20:57:08 +0800

    Fix error display thumbnails after scrolling list
    Signed-off-by: Shaka Huang <shaka@cs03.org>

--- src/com/proinlab/gdlapp/ScheduleListViewAdapter.java
+++ src/com/proinlab/gdlapp/ScheduleListViewAdapter.java
@@ -4,13 +4,14 @@ package com.proinlab.gdlapp;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.concurrent.RejectedExecutionException;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.Handler;
-import android.os.Message;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -25,8 +26,6 @@ class ScheduleListViewAdapter extends BaseAdapter implements OnClickListener {
     private LayoutInflater Inflater;
     private ArrayList<ArrayList<String>> arSrc;
     private int layout;
-    private Bitmap[] bitmap;
-    private ImageView[] Thumbnail;
 
     public static final int ARRAY_INDEX_TITLE = 0;
     public static final int ARRAY_INDEX_DATE = 1;
@@ -38,9 +37,6 @@ class ScheduleListViewAdapter extends BaseAdapter implements OnClickListener {
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         arSrc = aarSrc;
         layout = R.layout.schedule_content;
-
-        Thumbnail = new ImageView[aarSrc.size()];
-        bitmap = new Bitmap[aarSrc.size()];
     }
 
     public int getCount() {
@@ -55,14 +51,15 @@ class ScheduleListViewAdapter extends BaseAdapter implements OnClickListener {
         return position;
     }
 
-    public View getView(int position, View convertView, ViewGroup parent) {
+    @SuppressLint("NewApi")
+	public View getView(int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
             convertView = Inflater.inflate(layout, parent, false);
         }
 
-        Thumbnail[position] = (ImageView) convertView
+        ImageView thumbnail = (ImageView) convertView
                 .findViewById(R.id.schedule_content_thumbnails);
-        Thumbnail[position].setImageBitmap(null);
+        thumbnail.setImageBitmap(null);
 
         TextView title = (TextView) convertView
                 .findViewById(R.id.schedule_content_name);
@@ -75,42 +72,66 @@ class ScheduleListViewAdapter extends BaseAdapter implements OnClickListener {
         convertView.setTag(position);
         convertView.setOnClickListener(this);
 
-        process(arSrc.get(position).get(ARRAY_INDEX_THUMBNAIL), position);
+        final ThumbnailAsyncTask oldTask = (ThumbnailAsyncTask) thumbnail.getTag();
+		if (oldTask != null) {
+			oldTask.cancel(false);
+		}
 
+		final ThumbnailAsyncTask task = new ThumbnailAsyncTask(thumbnail);
+		thumbnail.setTag(task);
+		try {
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+				task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, position);
+			} else {
+				task.execute(position);
+			}
+		} catch (RejectedExecutionException e) {
+		} catch (OutOfMemoryError e) {
+
+		}
+        
         return convertView;
     }
 
-    private final Handler handler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            if (bitmap[msg.what] == null)
-                return;
-            Thumbnail[msg.what].setImageBitmap(bitmap[msg.what]);
-            Thumbnail[msg.what].setBackgroundDrawable(null);
-        }
-    };
+	private class ThumbnailAsyncTask extends AsyncTask<Integer, Void, Bitmap> {
+		private final ImageView mTarget;
 
-    private void process(final String url, final int position) {
-        new Thread() {
-            @Override
-            public void run() {
-                try {
-                    InputStream is = new URL(url).openStream();
-                    bitmap[position] = BitmapFactory.decodeStream(is);
-                    is.close();
+		public ThumbnailAsyncTask(ImageView target) {
+			mTarget = target;
+		}
 
-                    handler.post(new Runnable() {
-                        public void run() {
-                            handler.sendEmptyMessage(position);
-                        }
-                    });
-                } catch (Exception e) {
+		@Override
+		protected void onPreExecute() {
+			mTarget.setTag(this);
+		}
 
-                }
-            }
-        }.start();
-    }
+		@Override
+		protected Bitmap doInBackground(Integer... params) {
+			final int pos = params[0];
+			final String url = arSrc.get(pos).get(ARRAY_INDEX_THUMBNAIL);
 
+			try {
+				InputStream is = new URL(url).openStream();
+				final Bitmap result = BitmapFactory.decodeStream(is);
+				is.close();
+				return result;
+			} catch (Exception e) {
+
+			}
+
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Bitmap result) {
+			if (mTarget.getTag() == this) {
+				mTarget.setImageBitmap(result);
+				mTarget.setBackgroundDrawable(null);
+				mTarget.setTag(null);
+			}
+		}
+	}
+    
     @Override
     public void onClick(View v) {
 
